07386fd379905b05373c0f46661b5c3384d9eace
abdul-rehman-2050 <abdul.rehman.vud@gmail.com>
2020-12-31 22:34:07 +0500

    Styles applied

--- src/PostCard.js
+++ src/PostCard.js
@@ -1,25 +1,44 @@
 import React from "react";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
-import Collapse from "@material-ui/core/Collapse";
-import Avatar from "@material-ui/core/Avatar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  Avatar,
+  IconButton,
+  Typography
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { red } from "@material-ui/core/colors";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import ShareIcon from "@material-ui/icons/Share";
+import { FavoriteIcon, ShareIcon } from "@material-ui/icons";
+
 export default function PostCard(props) {
+  const classes = makeStyles(theme => ({
+    root: {
+      maxWidth: 345
+    },
+    content: {
+      height: 300,
+      overflow: auto,
+      backgroundColor: props.bgcolor[400]
+    },
+    avatar: {
+      backgroundColor: red[500]
+    }
+  }))();
+
   return (
     <Card>
       <CardHeader
-        avatar={<Avatar aria-label="recipe">R</Avatar>}
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            R
+          </Avatar>
+        }
         title={props.book}
         subheader="September 14, 2016"
       />
-      <CardContent>
+      <CardContent className={classes.content}>
         <Typography variant="body2" color="textSecondary" component="p">
           {props.content}
         </Typography>
