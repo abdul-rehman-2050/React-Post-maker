d55369aec33b6e880193f6230a754b4411b932e7
abdul-rehman-2050 <supermind2002@gmail.com>
2021-01-01 00:06:35 +0500

    appbar changed to sticky

--- src/TopBar.js
+++ src/TopBar.js
@@ -11,7 +11,7 @@ import MenuIcon from "@material-ui/icons/Menu";
 
 export default function TopBar() {
   return (
-    <AppBar position="static">
+    <AppBar position="sticky">
       <Toolbar>
         <IconButton edge="start" color="inherit" aria-label="menu">
           <MenuIcon />
