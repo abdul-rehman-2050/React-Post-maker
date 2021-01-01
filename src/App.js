0c1703ce9b606d89e9eff8e951c6ef4def732c04
abdul-rehman-2050 <abdul.rehman.vud@gmail.com>
2021-01-01 22:32:12 +0500

    1 file deleted

--- src/App.js
+++ src/App.js
@@ -98,7 +98,7 @@ export default function App() {
           </Grid>
 
           <Grid item md={6} sm={8}>
-            <CreatePost />
+            
             <PostCard
               content={post.content}
               book={post.book}
