84f486d5f64acbc5df457af93a58f9ec09c9f059
abdul-rehman-2050 <supermind2002@gmail.com>
2021-01-01 23:42:13 +0500

    create post card

--- src/CreatePost2.js
+++ src/CreatePost2.js
@@ -1,5 +1,59 @@
 import React from "react";
+import {
+	Card,
+	CardContent,
+	CardMedia,
+	IconButton,
+	Typography
+	}	from '@material-ui/core';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+	marginBottom: 16
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 151,
+  },
+  controls: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+  },
+  playIcon: {
+    height: 38,
+    width: 38,
+  },
+}));
+
 
 export default function CreatePost() {
-  return <input />;
+	 const classes = useStyles();
+  return (
+  <Card className={classes.root}>
+  <div className={classes.details}>
+  <CardContent className={classes.content}>
+          <Typography component="h5" variant="h5">
+            Live From Space
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+            Mac Miller
+          </Typography>
+        </CardContent>
+  </div>
+  </Card>
+  
+  
+  );
+  
+  
 }
