bde24b4c5d4c58f2f77b52ccd6785e4f9753bac9
abdul-rehman-2050 <supermind2002@gmail.com>
2021-01-02 01:14:53 +0500

    Create post Card Updated

--- src/CreatePost2.js
+++ src/CreatePost2.js
@@ -1,80 +1,92 @@
 import React from "react";
 import {
-	Card,
-	CardHeader,
-	CardActions,
-	CardContent,
-	CardMedia,
-	IconButton,
-	Typography,
-	Button,TextField,Grid
-	}	from '@material-ui/core';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+  Card,
+  CardHeader,
+  CardActions,
+  CardContent,
+  CardMedia,
+  IconButton,
+  Typography,
+  InputBase,
+  TextareaAutosize,
+  Button,
+  TextField,
+  Grid,
+  Divider
+} from "@material-ui/core";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { red } from "@material-ui/core/colors";
-import RoundedAvatar from "./RoundedAvatar.js"
+import { borders } from "@material-ui/system";
+import RoundedAvatar from "./RoundedAvatar.js";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
-	marginBottom: 16
+    display: "flex",
+    marginBottom: 16
   },
   details: {
-    display: 'flex',
-    flexDirection: 'column',
+    flex: "1 0 auto",
+    flexDirection: "column"
   },
   content: {
-    flex: '1 0 auto',
+    flex: "1 0 auto"
   },
   cover: {
-    width: 151,
+    width: 151
   },
   controls: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     paddingLeft: theme.spacing(1),
-    paddingBottom: theme.spacing(1),
+    paddingBottom: theme.spacing(1)
   },
   playIcon: {
     height: 38,
-    width: 38,
+    width: 38
   },
+  pillButton:{
+	marginLeft: "auto",
+    borderRadius: 50
+  }
 }));
 
-
 export default function CreatePost() {
-	 const classes = useStyles();
+  const classes = useStyles();
   return (
-  <Card className={classes.root}>  
-  <CardHeader
+    <Card className={classes.root}>
+      <CardHeader
         avatar={
-          <RoundedAvatar displaychar={'R'} bgcolor={red[500]} isonline={true}/>
-          
+          <RoundedAvatar displaychar={"R"} bgcolor={red[500]} isonline={true} />
         }
-        
       />
-      
-	  <CardContent className={classes.content}>		        
-		<TextField 
-			id="outlined-basic" 
-			label="Write New Post"
-			variant="outlined" 		
-			multiline
-			rowsMax={6}			
-			fullWidth/>	
+      <div className={classes.details}>
+        <CardContent className={classes.content}>
+          <TextField
+            placeholder="Write New Post..."
+            multiline
+            rows={4}
+			variant="outlined"
+            inputProps={{ "aria-label": "naked" }}
+            fullWidth
+          />
+		  <TextField
+		   placeholder="Refference:"
+		   variant="outlined"
+		   fullWidth
+		   />
+        </CardContent>
 
-	  </CardContent>	   
-	  <CardActions>
-		<Button variant="contained" color="primary">    
-           Create
-		</Button> 
-		
-	  </CardActions>
-	 
-	
-  </Card>
-  
-  
+        
+        <CardActions>
+          <Button
+            variant="contained"
+            color="primary"
+            className = {classes.pillButton}
+          >
+            Create
+          </Button>
+        </CardActions>
+      </div>
+    </Card>
   );
-  
-  
 }
