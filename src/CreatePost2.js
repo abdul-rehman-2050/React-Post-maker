6877cd588e6c87ed9b7b6fe8ce62c87f2f5d2dda
abdul-rehman-2050 <abdul.rehman.vud@gmail.com>
2021-01-01 22:39:34 +0500

    1 file modified, 1 added

---
+++ src/CreatePost2.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect, useContext } from "react";
+import {
+  Paper,
+  Grid,
+  CircularProgress,
+  TextField,
+  FormControl,
+  InputBase,
+  InputLabel,
+  Collapse,
+  Button
+} from "@material-ui/core";
+import { withStyles, makeStyles, fade } from "@material-ui/core/styles";
+
+export default function CreatePost() {
+  return <input type="text" />;
+}
