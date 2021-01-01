3b02dd0f59829a31f2cf64376452df6a3c130569
abdul-rehman-2050 <supermind2002@gmail.com>
2021-01-01 21:40:30 +0500

    Rounded Avatar with online overlap badge added

--- src/RoundedAvatar.js
+++ src/RoundedAvatar.js
@@ -2,12 +2,57 @@
 
 */
 import React from "react";
-import Avatar from "material-ui/Avatar";
+import Avatar from "@material-ui/core/Avatar";
+import Badge from "@material-ui/core/Badge";
+import {green } from "@material-ui/core/colors";
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 
-export default (RoundAvatar = props => {
+
+const StyledBadge = withStyles((theme) => ({
+  badge: {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: '$ripple 1.2s infinite ease-in-out',
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}))(Badge);
+
+
+export default function RoundAvatar(props){
   const onlineStyle = {
     border: "3px solid #8FBC8F",
     backgroundColor: props.bgcolor
   };
-return <Avatar style={props.isonline ? onlineStyle : null} >{props.displaychar}</Avatar>;
-});
+return (
+	<StyledBadge
+        overlap="circle"
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        variant="dot"
+      >          	
+	<Avatar style={props.isonline ? onlineStyle : null} >{props.displaychar}</Avatar>
+	</StyledBadge>
+);
+}
