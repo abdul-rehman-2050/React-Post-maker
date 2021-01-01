9292b4838ce4891f3000698e2e46fecbbe7de168
abdul-rehman-2050 <supermind2002@gmail.com>
2021-01-01 18:45:15 +0500

    avatar changed

--- src/RoundedAvatar.js
+++ src/RoundedAvatar.js
@@ -1,23 +1,13 @@
 /*https://github.com/dnsndrvsk/NT-Social-Network/blob/master/client/src/components/RoundAvatar/index.js
 
 */
-import React from 'react'
-import PropTypes from 'prop-types'
-import Avatar from 'material-ui/Avatar'
+import React from "react";
+import Avatar from "material-ui/Avatar";
 
-const onlineStyle = {
-  border: '3px solid #8FBC8F'
-}
-
-const RoundAvatar = (props) => (
-  <Avatar
-    src={props.user.avatar}
-    style={props.user.online.currently ? onlineStyle : null}
-  />
-)
-
-RoundAvatar.propTypes = {
-  user: PropTypes.object.isRequired
-}
-
-export default RoundAvatar
+export default (RoundAvatar = props => {
+  const onlineStyle = {
+    border: "3px solid #8FBC8F",
+    backgroundColor: props.bgcolor
+  };
+  return <Avatar style={props.isonline ? onlineStyle : null} />;
+});
