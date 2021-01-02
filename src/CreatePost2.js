f61dc1087a58493f7592b538b107061d340d9672
abdul-rehman-2050 <supermind2002@gmail.com>
2021-01-02 13:23:30 +0500

    unnessasary imports removed

--- src/CreatePost2.js
+++ src/CreatePost2.js
@@ -1,41 +1,27 @@
 import React from "react";
 import {
   Paper,
-  Card,
-  CardHeader,
-  CardActions,
-  CardContent,
-  CardMedia,
   IconButton,
-  Typography,
-  InputBase,
-  TextareaAutosize,
   Button,
   TextField,
   Grid,
   Divider,
-  Tooltip 
+  Tooltip
 } from "@material-ui/core";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
-import { red } from "@material-ui/core/colors";
-import { borders } from "@material-ui/system";
-import RoundedAvatar from "./RoundedAvatar.js";
-import FormatColorFillIcon from '@material-ui/icons/FormatColorFill';
-import ColorLensTwoToneIcon from '@material-ui/icons/ColorLensTwoTone';
+import ColorLensTwoToneIcon from "@material-ui/icons/ColorLensTwoTone";
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: "flex",
     marginBottom: 16,
-	overflow:"auto",
-	paddingRight: 16,
-	paddingLeft: 16,
-	paddingTop: 16,
-	paddingBottom: 16,
-	
+    overflow: "auto",
+    paddingRight: 16,
+    paddingLeft: 16,
+    paddingTop: 16,
+    paddingBottom: 16
   },
   details: {
-    
     flexDirection: "column"
   },
   content: {
@@ -46,22 +32,21 @@ const useStyles = makeStyles(theme => ({
   },
   controls: {
     display: "flex",
-    alignItems: "center",  
-    paddingBottom: theme.spacing(1),
-	
+    alignItems: "center",
+    paddingBottom: theme.spacing(1)
   },
- 
-  pillButton:{
-	marginLeft: "auto",
+
+  pillButton: {
+    marginLeft: "auto",
     borderRadius: 50,
-	paddingLeft: theme.spacing(8),
-	paddingRight: theme.spacing(8),
-	marginLeft: "auto"
+    paddingLeft: theme.spacing(8),
+    paddingRight: theme.spacing(8),
+    marginLeft: "auto"
   },
-  
-  iconsControls:{
-	  color:  theme.palette.warning.main,
-	  fontSize: 25 
+
+  iconsControls: {
+    color: theme.palette.warning.main,
+    fontSize: 25
   }
 }));
 
@@ -69,60 +54,43 @@ export default function CreatePost() {
   const classes = useStyles();
   return (
     <Paper className={classes.root}>
-      
-		
-		<div className={classes.details}>
-		
-		
-          <TextField
-            placeholder="Write New Post..."
-            multiline
-            rows={4}
-			variant="outlined"
-            inputProps={{ "aria-label": "naked" }}
-            fullWidth
-          />
-		
-		<div className={classes.controls}>
-		  <TextField
-		   placeholder="Author:"
-		   variant="outlined"
-		   
-		   />
-		
-		 <TextField
-		   placeholder="Book"
-		   variant="outlined"		   
-		   />
-		 <TextField
-		   placeholder="Chapter"
-		   variant="outlined"		   
-		   />  
-		 <TextField
-		   placeholder="Page"
-		   variant="outlined"		   
-		   />		
-		
-			</div>
-		
-			<div style={{display:"flex"}}>
-        <Tooltip title="Background Color">
-			<IconButton >
-				<ColorLensTwoToneIcon aria-label="Background Color" className={classes.iconsControls}/>
-			</IconButton>
-		</Tooltip>
-		  
+      <div className={classes.details}>
+        <TextField
+          placeholder="Write New Post..."
+          multiline
+          rows={4}
+          variant="outlined"
+          inputProps={{ "aria-label": "naked" }}
+          fullWidth
+        />
+
+        <div className={classes.controls}>
+          <TextField placeholder="Author:" variant="outlined" />
+
+          <TextField placeholder="Book" variant="outlined" />
+          <TextField placeholder="Chapter" variant="outlined" />
+          <TextField placeholder="Page" variant="outlined" />
+        </div>
+
+        <div style={{ display: "flex" }}>
+          <Tooltip title="Background Color">
+            <IconButton>
+              <ColorLensTwoToneIcon
+                aria-label="Background Color"
+                className={classes.iconsControls}
+              />
+            </IconButton>
+          </Tooltip>
+
           <Button
             variant="contained"
             color="primary"
-            className = {classes.pillButton}
+            className={classes.pillButton}
           >
             Create
           </Button>
-		  
-		  </div>
-		</div>
-      
+        </div>
+      </div>
     </Paper>
   );
 }
